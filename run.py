492fa4ba675f47ba24ccfb731844ee4ea5e91e14
zhuyuhe <zhuyuhe@kuaishou.com>
2022-06-25 17:32:06 +0800

    add wsgi server

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 # 创建应用实例
+from gevent import pywsgi
 import sys
 
 from wxcloudrun import app
 
 # 启动Flask Web服务
 if __name__ == '__main__':
-    app.run(host=sys.argv[1], port=sys.argv[2])
+    #app.run(host=sys.argv[1], port=sys.argv[2])
+    server = pywsgi.WSGIServer((sys.argv[1], int(sys.argv[2])), app)
+    server.serve_forever()
